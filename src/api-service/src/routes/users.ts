3aa6c9e98bb2a721bdf28f2c250748c6390053f9
Mohammed G. Al-Mahdawi <48254077+Mohido@users.noreply.github.com>
2023-02-14 03:46:12 +0100

    API & Postgres Services Connection (#16)
    * Established Connection Between API & Postgres Services
    * Removed Unused Imports & Interfaces
    * Added CRG Application Database Backup

--- src/api-service/src/routes/users.ts
+++ src/api-service/src/routes/users.ts
@@ -2,24 +2,16 @@
 import { Router } from "express";
 import RouterPromise from 'express-promise-router';
 import { Logger } from "tslog";
-import config from "../settings/config";
-import { ClientResponse } from "../utils/keycloak-client";
 
-const router :Router = RouterPromise();
+
 const logger: Logger<Router> = new Logger();
+const router :Router = RouterPromise();
 
 
 
 
 router.post('/:username/login', async (req, res, next) => {
-    let clientResponse : ClientResponse = await config.kc_client.createUserTokens(req.params.username, req.body.password);
-    if(clientResponse.result){
-        res.status(200).send({tokens: clientResponse.result});
-    }
-    else{
-        logger.error(clientResponse.error);
-        res.status(500).send({error: clientResponse.error});
-    }
+    
 });
 
 
--- src/api-service/src/settings/config.ts
+++ src/api-service/src/settings/config.ts
@@ -8,6 +8,7 @@
  */
 import {MemoryStore} from 'express-session';
 import { KeycloakClient } from '../utils/keycloak-client';
+import { PostgreTransactioner } from '../utils/postgres-trans';
 
 namespace API{
    
@@ -19,7 +20,8 @@ namespace API{
         tls_path?: string,
         memory_store?: any,
         session_secret? : string,
-        kc_client?: KeycloakClient
+        kc_client?: KeycloakClient,
+        pg_client? : PostgreTransactioner
     }
 }
 
@@ -35,7 +37,6 @@ export default {
     api_port: process.env.API_PORT || 5001,
     api_dns: process.env.API_DNS || "localhost",
     auth_dns:  process.env.AUTH_DNS || "localhost",
-    postgre_dns: process.env.POSTGRE_DNS || "localhost",
     memory_store : memory_store,
     session_secret : process.env.SESSION_SECRET || "this is a secret",
     kc_client : new KeycloakClient({
@@ -43,5 +44,12 @@ export default {
         client_secret: process.env.KC_CLIENT_SECRET,
         client_realm: process.env.KC_REALM,
         keycloak_url: `http://${process.env.AUTH_DNS}:${process.env.AUTH_PORT || 8081}/auth/`
-    }, memory_store)
+    }, memory_store),
+    pg_client : new PostgreTransactioner({
+        user: process.env.DB_USER,                  // e.g: 'dbuser',
+        host: process.env.POSTGRE_DNS,              // e.g: 'database.server.com',
+        database: process.env.DB_NAME,              // e.g: 'mydb',
+        password: process.env.DB_PASSWORD,          // e.g: 'secretpassword',
+        port: parseInt(process.env.DB_PORT)         // e.g: 3211,
+    })
 } as API.ServiceConfiguration;
