68dd21a389c37603f3c2648a129b37dc57fdb4e0
Mohido <m.ghaithalmahdawi@gmail.com>
2023-02-26 02:04:21 +0100

    Fixed Project Title Overflowed Text

--- src/gui-service/src/components/project/index.tsx
+++ src/gui-service/src/components/project/index.tsx
@@ -21,8 +21,11 @@ export const ProjectBox = component$((props: SummaryProjectData)=>{
         <div class="project-box">
             <div class="project-name">{props.name}</div>
             <div class="project-description">{props.short_description}</div>
-            <div class="project-update">Updated: {props.last_update}</div>
-            <div class="project-update">Created: {props.last_update}</div>
+            <div class="project-extra">
+                <div>Updated: {props.last_update}</div>
+                <div>Created: {props.last_update}</div>
+                <div>Owner: Mohido</div>
+            </div>
         </div>
     );
 });
