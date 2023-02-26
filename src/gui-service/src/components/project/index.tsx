95afa76379e9e994360676377aee620fbb47aa99
Mohido <m.ghaithalmahdawi@gmail.com>
2023-02-26 18:24:36 +0100

    Finished the Basic Design of the Projects Viewer. Some Artistic Enhancement is Required

--- src/gui-service/src/components/project/index.tsx
+++ src/gui-service/src/components/project/index.tsx
@@ -19,13 +19,48 @@ export const ProjectBox = component$((props: SummaryProjectData)=>{
     useStylesScoped$(projectCSS);
     return (
         <div class="project-box">
-            <div class="project-name">{props.name}</div>
-            <div class="project-description">{props.short_description}</div>
-            <div class="project-extra">
-                <div>Updated: {props.last_update}</div>
-                <div>Created: {props.last_update}</div>
-                <div>Owner: Mohido</div>
+            <a class="project-box-hoverer">
+                <img src="/images/projects-placeholder-png.png" class="project-box-image"/>
+                <div class="project-box-title">{props.name}</div>
+                <div class="project-box-description">{props.short_description}
+                    {/* <div>{props.short_description}</div> */}
+                    {/* <div class="project-box-description-fade"></div> */}
+                </div>
+                
+                <div class="project-box-extra-1">
+                    <div>
+                        <div>Updated</div>
+                        <div>{props.last_update}</div>
+                    </div>
+                    <div>
+                        <div>Owner</div>
+                        <div>Mohido</div>
+                    </div>
+                </div>
+            </a>
+            
+
+            <div class="project-box-extra-2" >
+                <div title="Active Users">
+                <i class="fa-solid fa-chart-line" ></i>
+                    <span>2</span>
+                </div>
+                <div title="Characters Count">
+                <i class="fa-solid fa-network-wired" ></i>
+                    <span>15</span>
+                </div>
+                <div title="Project Collaborators">
+                    <i class="fa-solid fa-user" ></i>
+                    <span>1412</span>
+                </div>
+                <div title="Import/Download Project">
+                    <button>
+                        <i class="fa-solid fa-download" ></i>
+                    </button>
+                </div>
+
             </div>
+
         </div>
     );
 });
--- src/gui-service/src/routes/main/projects/index.tsx
+++ src/gui-service/src/routes/main/projects/index.tsx
@@ -1,16 +1,19 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useStore, useStylesScoped$ } from "@builder.io/qwik";
 import Menu from "~/components/menu"
 import Profile from "~/components/profile";
 import { ProjectBox } from "~/components/project";
 import Search from "~/components/search";
-import {Content} from "~/containers/content";
+import contentCSS from "./index.css?inline"
 
 export default component$((props)=>{
     
+    useStylesScoped$(contentCSS);
+
+
     const projectsList = [
         {id:1, name: "project Alpha", short_description: "just a simple project", last_update:"10/Feb/3030"},
         {id:2, name: "Best Game", short_description: "just a simple project", last_update:"10/Feb/3030"},
-        {id:3, name: "Long Very Best Game just very very very long d", short_description: "just very very very long descript a simple project", last_update:"10/Feb/3030"},
+        {id:3, name: "Long Very Best Game just very very very long d", short_description: "just very very very long descript a simple project just very very very long descript a simple project just very very very long descript a simple project just very very very long descript a simple project  just very very very long descript a simple project", last_update:"10/Feb/3030"},
         {id:4, name: "project Alpha", short_description: "just a simple project", last_update:"10/Feb/3030"},
         {id:5, name: "Best", short_description: "just a simple project", last_update:"10/Feb/3030"}
     ]
@@ -21,13 +24,17 @@ export default component$((props)=>{
             <Menu></Menu>
             <Profile></Profile>
             <Search></Search>
-            <Content>
-            {
-                projectsList.map((el)=>{
-                    return <ProjectBox key={el.id} id={el.id} name={el.name} short_description={el.short_description} last_update={el.last_update}/>
-                })
-            }
-            </Content>
+            <div class="projects-container">
+                <h1 class="projects-header">Current Projects</h1>
+                <div class="projects-content">
+                {
+                    projectsList.map((el)=>{
+                        return <ProjectBox key={el.id} id={el.id} name={el.name} short_description={el.short_description} last_update={el.last_update}/>
+                    })
+                }
+                </div>
+            </div>
+           
             
 
         </div>
